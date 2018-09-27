2f655e0be69bc0461883f6081fbc9cefe385b26d
AlejandroSuau <alejandro.suau@gmail.com>
2018-09-27 15:40:55 +0200

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,15 +1,18 @@
 import helper
 import encryptation_algorithms
 
+alphabet = helper.create_english_alphabet()
+caps_alphabet = helper.create_english_caps_alphabet()
+
 # Vigenere algorithm #
 word = "UYINMTRMZWTRGZ"
 key = "BRAVE"
 correct_solution = "THISISAMESSAGE"
 solution = encryptation_algorithms.vigenere_algorithm(caps_alphabet, word, key, "decrypt")
-assert solution == correct_solution
+assert correct_solution == solution
 
 word = "THISISAMESSAGE"
 key = "BRAVE"
-correct_solution = encryptation_algorithms.vigenere_algorithm(caps_alphabet, word, key, "encrypt")
-solution = "UYINMTRMZWTRGZ"
-assert solution == correct_solution
+correct_solution = "UYINMTRMZWTRGZ"
+solution = encryptation_algorithms.vigenere_algorithm(caps_alphabet, word, key, "encrypt")
+assert correct_solution == solution
