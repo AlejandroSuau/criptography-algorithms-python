41f1d142b3c32254fcb95ab26a4cf5ce826eb2dd
AlejandroSuau <alejandro.suau@gmail.com>
2018-09-27 15:37:51 +0200

    Update criptography-algorithms.py

--- criptography-algorithms.py
+++ criptography-algorithms.py
@@ -1,11 +1,23 @@
-
-def create_alphabet_list():
-    alphabet = []
+def vigenere_algorithm(alphabet = [], message = "", key = "", method = "encrypt"):
+    message_result = ""
+    alphabet_len = len(alphabet)
+    i = 0
     
-    i = ord('a')
-    last_it = ord('z')
-    while(i <= last_it):
-        alphabet.append(chr(i))
-        i += 1
+    for character in message:
+        key_idx = alphabet.index(key[i])
+        if method == "decrypt":
+            key_idx *= -1
+            
+        char_idx = alphabet.index(character) + key_idx
+        
+        if char_idx < 0 or char_idx >= alphabet_len:
+            char_idx %= alphabet_len
+        
+        if i == len(key)-1:
+            i = 0
+        else:
+            i += 1
+        
+        message_result += alphabet[char_idx]
     
-    return alphabet
+    return message_result
