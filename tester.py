e197bf9703756c1f18b60a3108120be947464a7e
AlejandroSuau <alejandro.suau@gmail.com>
2018-09-27 16:09:55 +0200

    Update tester.py

--- tester.py
+++ tester.py
@@ -4,15 +4,30 @@ import encryptation_algorithms
 alphabet = helper.create_english_alphabet()
 caps_alphabet = helper.create_english_caps_alphabet()
 
-# Vigenere algorithm #
+# Caesar Cipher Unit Testing #
+word = "HOWTOSHAREASECRET"
+key = 1
+correct_solution = "IPXUPTIBSFBTFDSFU"
+solution = encryptation_algorithms.caesar_cipher(caps_alphabet, word, key, "encrypt")
+assert correct_solution == solution
+
+word = "IPXUPTIBSFBTFDSFU"
+key = 1
+correct_solution = "HOWTOSHAREASECRET"
+solution = encryptation_algorithms.caesar_cipher(caps_alphabet, word, key, "decrypt")
+assert correct_solution == solution
+# End Caesar Ciper Unit Testing #
+
+# Vigenere Cipher Unit Testing #
 word = "UYINMTRMZWTRGZ"
 key = "BRAVE"
 correct_solution = "THISISAMESSAGE"
-solution = encryptation_algorithms.vigenere_algorithm(caps_alphabet, word, key, "decrypt")
-assert correct_solution == solution
+solution = encryptation_algorithms.vigenere_cipher(caps_alphabet, word, key, "decrypt")
+assert solution == correct_solution
 
 word = "THISISAMESSAGE"
 key = "BRAVE"
-correct_solution = "UYINMTRMZWTRGZ"
-solution = encryptation_algorithms.vigenere_algorithm(caps_alphabet, word, key, "encrypt")
-assert correct_solution == solution
+correct_solution = encryptation_algorithms.vigenere_cipher(caps_alphabet, word, key, "encrypt")
+solution = "UYINMTRMZWTRGZ"
+assert solution == correct_solution
+# End Vigenere Cipher Unit Testing #
